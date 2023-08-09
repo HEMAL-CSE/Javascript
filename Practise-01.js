ca3e1a4e5da1946d62aac0daf7123ba88e0cd2d2
HEMAL-CSE <habibur15-3697@diu.edu.bd>
2023-08-09 07:42:00 +0600

    practise

---
+++ Practise-01.js
@@ -0,0 +1,17 @@
+
+//Problem-03
+var input1 = "I am going to be";
+var input2 = "an awesome web developer";
+var output = input1.concat(" ", input2);
+var output1 = input1 + " " + input2;
+console.log(output);
+console.log(output1);
+
+
+
+// Practise problem-04
+var number = 119;
+var divider = 5;
+var result = number % divider;
+console.log(result);
+
