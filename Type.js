90dbd23a6054ce71441a2edd9546018b908c1f42
HEMAL-CSE <habibur15-3697@diu.edu.bd>
2023-08-09 06:37:37 +0600

    3rd

---
+++ Type.js
@@ -0,0 +1,32 @@
+//Type related problem.
+var number = 100;
+console.log(number);
+console.log(typeof number);
+
+var name = "Habibur";
+console.log(typeof 'name');
+
+var isHappy = true;
+console.log(typeof isHappy);
+
+var romantic;
+console.log(romantic);
+
+// After decimal to create fixed, we use toFixed(). var= var.toFixed(2), 2 means After decimal 2 placee.
+var number1 = 0.8;
+var number2 = 0.4
+var sum = number1 + number2;
+sum= sum.toFixed(2);
+console.log(sum);
+
+var n1=0.3;
+var n2 = 0.1;
+var sub = n1 - n2;
+sub = sub.toFixed(2);
+console.log(sub);
+
+// reminder
+
+var num1 = 502;
+var num2 = 150;
+console.log(num1 % num2);
